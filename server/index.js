b6a674fc02b1776a3605e2c8ffdfa2fd03957169
alesin <alesintipler@gmail.com>
2020-06-02 23:16:21 -0400

    handling 500 errors

--- server/index.js
+++ server/index.js
@@ -21,4 +21,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+// *** ERROR HANDLING middleware
+app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error')
+})
+
 module.exports = app
