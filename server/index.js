7642ad64b06fa2619a3c0d5c259ffbf59b6f8042
alesin <alesintipler@gmail.com>
2020-06-02 22:25:55 -0400

    settled on MORGAN for logging middleware

--- server/index.js
+++ server/index.js
@@ -1,2 +1,6 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const morgan = require('morgan');  // *** LOGGING middleware
+
+app.use(morgan('dev'))   // *** LOGGING middleware
+
