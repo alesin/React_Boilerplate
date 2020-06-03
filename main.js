854b1f45d9bea215a7841723cb3c6260f212ef28
alesin <alesintipler@gmail.com>
2020-06-02 23:32:41 -0400

    added route to start server

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = require('./server')
+
+// *** very useful setup for deployment on Heroku!
+const port = process.env.PORT || 3040
+
+app.listen(port, () => {
+  console.log("Knock, knock")
+  console.log("Who's there?")
+  console.log(`Your server, listening on port ${port}`)
+})
