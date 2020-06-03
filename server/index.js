728df6a989565728c23099309088a7e51d91c563
alesin <alesintipler@gmail.com>
2020-06-02 22:37:28 -0400

    added middleware: req.body parsing & static files

--- server/index.js
+++ server/index.js
@@ -3,4 +3,11 @@ const app = express()
 const morgan = require('morgan');  // *** LOGGING middleware
 
 app.use(morgan('dev'))   // *** LOGGING middleware
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))  // *** Customize MORGAN logging
 
+// *** BODY PARSING middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// *** STATIC middleware
+app.use(express.static(path.join(__dirname, '../public')))
