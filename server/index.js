5cd7b170763d5c2dff012778675070a022259f9a
alesin <alesintipler@gmail.com>
2020-06-08 21:38:52 -0400

    added session middleware: session store & passport

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {db, User} = require('./db')
 const morgan = require('morgan')  // *** LOGGING middleware
+const session = require('express-session')  // *** SESSION middleware
+const passport = require('passport')
 
 app.use(morgan('dev'))   // *** LOGGING middleware
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))  // *** Customize MORGAN logging
@@ -9,6 +12,42 @@ app.use(morgan('dev'))   // *** LOGGING middleware
 // *** BODY PARSING middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// *** SESSION middleware
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const dbStore = new SequelizeStore({db: db})
+
+dbStore.sync() // *** sync to create session table
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'A wildly insecure secret...',
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false
+}))
+
+// *** PASSPORT middleware (piggyback on SESSION middleware)
+app.use(passport.initialize())
+app.use(passport.session())
+
+// *** After FIND / CREATE user, we 'serialize' our user on the session
+passport.serializeUser((user, done) => {
+    try {
+        done(null, user.id)
+    } catch (error) {
+        done(error)
+    }
+})
+
+// *** Since we have 'serialized' user on session (with an id), use the id to find user and attach the user Obj to req.user
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id)
+        done(null, user)
+    } catch (error) {
+        done(error)
+    }
+})
 
 // *** STATIC middleware
 app.use(express.static(path.join(__dirname, '../public')))
