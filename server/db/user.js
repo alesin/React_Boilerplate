e1644d10badc145f70e2b6a2725816e85afdd47a
alesin <alesintipler@gmail.com>
2020-06-08 23:44:11 -0400

    added instance & class methods to salt & hash passwords

--- server/db/user.js
+++ server/db/user.js
@@ -1,8 +1,17 @@
 const Sequelize = require('sequelize')
 const db = require('./database')
+const crypto = require('crypto')
 
 const User = db.define('user', {
     username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    email: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
@@ -16,6 +25,9 @@ const User = db.define('user', {
             notEmpty: true
         }
     },
+    salt: {
+        type: Sequelize.STRING
+    },
     imageUrl: {
         type: Sequelize.STRING,
         defaultValue: 'default_user.png'
@@ -23,6 +35,44 @@ const User = db.define('user', {
     name: {
         type: Sequelize.STRING
     }
+}, {
+    hooks: {
+        beforeCreate: setSaltAndPassword,
+        beforeUpdate: setSaltAndPassword
+    }
 })
+
+// *** INSTANCE METHODS
+User.prototype.checkPassword = function (submittedPassword) {
+    //return true of false if the password matched the one on file
+    // this.hashPassword(submittedPassword, this.salt)
+    return this.password === this.User.hashPassword(submittedPassword, this.salt)
+}
+
+// *** CLASS METHODS
+User.generateSalt = function () {
+    return crypto.randomBytes(32).toString('base64')
+}
+
+User.hashPassword = function (plainText, salt) {
+    // accepts user's plaintext password & generated salt, returns the hash
+    const hash = crypto.createHash('sha256')
+    hash.update(plainText)
+    hash.update(salt)
+    return hash.digest('hex')
+}
+
+function setSaltAndPassword (user) {
+    // employ salt & hash when user creates password for 1st time OR when then change it
+    if (user.changed('password')) {
+        user.salt = User.generateSalt()
+        user.password = User.hashPassword(user.password, user.salt)
+    }
+}
+
+// User.beforeCreate(async (user, options) => {
+//     const hashedPassword = await hashPassword(user.password)
+//     user.password = hashedPassword
+// })
 
 module.exports = User
