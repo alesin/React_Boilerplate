58e56f49efd6e08f6bae502f91cfa50d2bc6031f
alesin <alesintipler@gmail.com>
2020-06-09 23:44:54 -0400

    added login route: through passport & with password check

---
+++ server/api/auth.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const {User} = require('../db')
+module.exports = router
+
+router.put('/login', async (req, res, next) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        if (!user) {
+            res.status(401).send('User not found.')
+        }
+        else if (!user.checkPassword(req.body.password)) {
+            res.status(401).send('Incorrect password.')
+        }
+        else {
+            // *** connect user to passport middleware
+            // *** http://www.passportjs.org/docs/login/
+            req.login(user, (error) => error ? next(error) : res.json(user))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 // *** ROUTES
 router.use('/users', require('./users'))
+// *** authentication router
+router.use('/auth', require('./auth'))
 
 // *** ERROR handling ==> no such route!
 router.use((req, res, next) => {
