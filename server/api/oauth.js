09b0c5a1aec4345f1411ad77cb9e8fb7e32b007e
alesin <alesintipler@gmail.com>
2020-06-11 04:37:31 -0400

    added callback route to manage Google request (aka user authenticated)

--- server/api/oauth.js
+++ server/api/oauth.js
@@ -9,3 +9,8 @@ module.exports = router
 // *** Google authentication and login (GET /auth/google)
 router.get('/', passport.authenticate('google', {scope: 'email'}))
 
+// *** handles the callback after Google has autheticated the user (GET /auth/google/callback)
+router.get('/callback', passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/login'
+}))
